a81beef1ebaa39bab5d34267b84d89498aabfefb
adhm <adhmnabi@gmail.com>
2025-01-20 17:10:48 +0200

    handel errors

--- src/components/Cards/ValueCard.tsx
+++ src/components/Cards/ValueCard.tsx
@@ -7,7 +7,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import AnimatedBackgroundBeams from "../Animations/AnimatedBackgroundBeams";
+// import AnimatedBackgroundBeams from "../Animations/AnimatedBackgroundBeams";
 
 function ValueCard({
   value,
