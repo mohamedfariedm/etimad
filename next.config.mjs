c699d78195b26d3a6cee2c2bba73cf8b48f19f58
adhm <adhmnabi@gmail.com>
2025-02-17 09:28:10 +0200

    handel updates locale

--- next.config.mjs
+++ next.config.mjs
@@ -2,29 +2,6 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["backend.advix.ai"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.pravatar.cc",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.simpleicons.org",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "backend.advix.ai",
-      },
-    ],
   },
 };
 
